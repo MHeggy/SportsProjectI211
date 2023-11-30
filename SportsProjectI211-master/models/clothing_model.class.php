188385e1430e557d03b37fccb4944487b864b4c1
chrforb <143653990+chrforb@users.noreply.github.com>
2023-11-29 19:02:15 -0500

    added searchClothes method
    this is what I have so far, probably imperfect.

--- SportsProjectI211-master/models/clothing_model.class.php
+++ SportsProjectI211-master/models/clothing_model.class.php
@@ -6,13 +6,14 @@
  * Description: File defining the ClothingModel Class.
  */
 
+require_once 'models/clothing.class.php';
 class ClothingModel {// start of ClothingModel class.
     //private attributes
     private $db, $dbConnection, $tblProduct;
     static private $_instance = NULL;
 
     //constructor
-    private function __construct() {
+    public function __construct() {
         $this->db = Database::getInstance();
         $this->dbConnection = $this->db->getConnection();
         $this->tblProduct = $this->db->getProductTable();
@@ -28,6 +29,7 @@ class ClothingModel {// start of ClothingModel class.
 
     //this retrieves all clothes in database, returns them in array if successful, false if failed
     public function listClothes() {
+        //sql to pull products from table -->needs work<--
         $sql = "SELECT * FROM " . $this->tblProduct;
 
         //execute sql
@@ -43,10 +45,47 @@ class ClothingModel {// start of ClothingModel class.
 
         //loop through results
         while ($obj = $query->fetch_object()) {
-            $clothing = new Clothing(stripslashes($obj->sport), stripslashes($obj->name),stripslashes($obj->price),stripslashes($obj->stock),stripslashes($obj->description),stripslashes($obj->image)) ;
+            $clothing = new Clothing(stripslashes($obj->product_id) ,stripslashes($obj->sport_name), stripslashes($obj->product_name),stripslashes($obj->price),stripslashes($obj->stock_quantity),stripslashes($obj->description),stripslashes($obj->image)) ;
 
             //set id for movie
-            $clothing->setId($obj->id);
+            $clothing->setId($obj->product_id);
+
+            //add to clothes array
+            $clothes[] = $clothing;
+        }
+        return $clothes;
+    }
+
+    //search database for relevant items relating to search terms
+    function searchClothes($terms) {
+        $terms = explode(" ", $terms);
+        //select statement for 'OR' search
+        $sql = "SELECT * FROM " . $this->tblProduct . " WHERE" ;
+
+        foreach ($terms as $term) {
+            $sql .= " product_name LIKE '%" . $term . "%'";;
+        }
+
+        //execute query
+        $query = $this->dbConnection->query($sql);
+
+        //if search failed, throw exception
+        //add code for exception
+
+        //search succeeded, but no movie was found.
+        if ($query->num_rows == 0) {
+            return 0;
+        }
+
+        //if clothes were found, create a clothing array
+        $clothes = array();
+
+        //loop through all returned searches
+        while ($obj = $query->fetch_object()) {
+            $clothing = new Clothing(stripslashes($obj->product_id) ,stripslashes($obj->sport_name), stripslashes($obj->product_name),stripslashes($obj->price),stripslashes($obj->stock_quantity),stripslashes($obj->description),stripslashes($obj->image)) ;
+
+            //set id for movie
+            $clothing->setId($obj->product_id);
 
             //add to clothes array
             $clothes[] = $clothing;
